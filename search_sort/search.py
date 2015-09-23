48839fdb4dfa386da5a67ba831c0e56ad7a213ac
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2015-09-23 15:23:27 -0700

    Binary search passes

--- search_sort/search.py
+++ search_sort/search.py
@@ -6,8 +6,8 @@ class Searches:
 		while i < len(list):
 			if list[i] == target:
 				return i
-			else: 
-				i += 1 
+			else:
+				i += 1
 		return -1
 
 		# for i, value in enumerate(list):
@@ -17,6 +17,41 @@ class Searches:
 
 
 	def binary(self, list, target):
-		# assumption that list is already sorted
-		pass
+		# List is sorted
+		min_index = 0
+		max_index = int(len(list) - 1)
+		while min_index <= max_index:
+			midpoint = int((min_index + max_index) / 2)
+			if target == list[midpoint]:
+				return midpoint
+			elif target < list[midpoint]:
+				max_index = midpoint - 1
+			elif target > list[midpoint]:
+				min_index = midpoint + 1
+		return -1
 
+	# Overthinking things: 
+	def inner_binary(self, list, target, min_index, max_index):
+		midpoint = int((max_index + min_index) / 2)
+
+		if max_index - min_index > 0:
+			if target == list[midpoint]:
+				return midpoint
+			elif target < list[midpoint]:
+				new_max = midpoint - 1
+				return self.inner_binary(list, target, min_index, new_max)
+			elif target > list[midpoint]:
+				new_min = midpoint + 1
+				return self.inner_binary(list, target, new_min, max_index)
+			else:
+				return "fail"
+		else:
+			return -1
+
+	def rec_binary(self, list, target):
+		# assumption that list is already sorted
+		min_index = 0
+		max_index = len(list) - 1
+
+		index = self.inner_binary(list, target, min_index, max_index)
+		return index
