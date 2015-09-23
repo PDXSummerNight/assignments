045cf407097705eaebaf5ed1a50bf5e0fe7abcb5
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2015-09-22 21:01:49 -0700

    Linear search tests pass

--- search_sort/search.py
+++ search_sort/search.py
@@ -1,8 +1,22 @@
 
 class Searches:
 
-    def linear(self, list, target):
-        pass
+	def linear(self, list, target):
+		i = 0
+		while i < len(list):
+			if list[i] == target:
+				return i
+			else: 
+				i += 1 
+		return -1
 
-    def binary(self, list, target):
-        pass
+		# for i, value in enumerate(list):
+		# 	if list[i] == target:
+		# 		return i
+		# return -1
+
+
+	def binary(self, list, target):
+		# assumption that list is already sorted
+		pass
+
