05688b9b01047bbd14031ba14eb27da07645c382
Jake Richmond <jakeorichmond@gmail.com>
2015-09-23 19:07:05 -0700

    Damn Binary Search

--- search_sort/search.py
+++ search_sort/search.py
@@ -2,7 +2,29 @@
 class Searches:
 
     def linear(self, list, target):
-        pass
+    	for i, value in enumerate(list):
+    		if value == target:
+    			return i
 
-    def binary(self, list, target):
-        pass
+
+
+
+    	return -1       
+
+def binary(self, list, target):
+	first = 0
+	last = len(list)-1
+	found = False
+	
+	while first <= last and not found:
+		midpoint = (first + last)//2
+		if list[midpoint] == target:
+			found = True
+
+		elif target < list[midpoint]:
+				last = midpoint - 1
+		
+		else:
+			first = midpoint + 1
+			
+			return found
