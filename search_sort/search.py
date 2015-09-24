d0aaf942e18ac8fea81091aea25fa45b714086c6
Tiffany <tar3820@gmail.com>
2015-09-23 21:09:07 -0700

    Added slides for Django. Updated Binary search, but it doesn't work yet.

--- search_sort/search.py
+++ search_sort/search.py
@@ -10,4 +10,27 @@ class Searches:
         return -1
 
     def binary(self, list, target):
-        pass
+        # set mid
+        min = 0
+        max = len(list) - 1
+
+
+        while min <= max:
+            # Else, if the halfway point is > or < target:
+            # Greater than: divide right half of list
+            mid = (min + max) // 2
+            print(list[mid])
+            if list[mid] == target:
+                return mid
+            elif list[mid] > target:
+                min = mid + 1
+                print("Bigger")
+
+            else:
+                # Less than: divide left half of list
+                max = mid - 1
+
+        print(min, max, mid)
+
+        # Didn't find what we were looking for
+        return -1
